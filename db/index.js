94974b0ed3cf5d74735209e9bbbd6f441bd53bcb
Tiago Almeida <thyagoeap@gmail.com>
2018-08-12 15:12:24 -0300

    uri database

--- db/index.js
+++ db/index.js
@@ -1,8 +1,7 @@
 'use strict';
 const { Client } = require('pg');
 
-//const connectionString = 'postgresql://postgres:root@localhost:5432/exames';
-const connectionString = 'postgresql://postgres:root@localhost:5432/shop';
+const connectionString = 'postgres://cyyycvzxvbtuij:3a59b6785bbf40c9a5bc8752c8ec2ef8f47adb565e4481e3c0005ad2c8622189@ec2-54-227-241-179.compute-1.amazonaws.com:5432/df9d1k06lkphtv';
 const client = new Client({connectionString});
 
 module.exports = client;
